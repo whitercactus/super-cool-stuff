df81f9f747d41cc4fd58696b7ac01d2c34b78007
TrashCan <61761890+whitercactus@users.noreply.github.com>
2022-01-02 01:41:52 -0800

    Update gg.sh

--- gg.sh
+++ gg.sh
@@ -2,3 +2,4 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         xdg-open "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         open "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+fi
