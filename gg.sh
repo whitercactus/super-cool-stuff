e73e16384ab170cc22a48447ec126430e1d21c22
TrashCan <61761890+whitercactus@users.noreply.github.com>
2022-01-02 01:34:42 -0800

    Update gg.sh

--- gg.sh
+++ gg.sh
@@ -1,1 +1,4 @@
-start "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        xdg-open "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        open "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
